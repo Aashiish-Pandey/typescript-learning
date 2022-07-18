80d062aa16b153b73cbec4223a2cfbc398e14f95
Aashiish-pandey <ash006pandey@email.com>
2022-07-18 09:17:04 +0530

    competed

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,64 @@
 "use strict";
+// Basic types
 let id = 5;
-id = "ashish";
+let company = "PS";
+let isPublished = true;
+let X = "Hello";
+X = 123;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
+//tuple
+let person = [1, "brad", true];
+// tuple array
+let employee;
+employee = [
+    [1, "ash"],
+    [2, "pan"],
+    [3, "day"],
+];
+// union
+let id1 = 22;
+// enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Left);
+const user = {
+    id: 1,
+    name: "Ashish"
+};
+// Type Assertion 
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
+// Functions
+function addNumber(x, y) {
+    return x + y;
+}
+console.log(addNumber(2, 3));
+// void
+function log(message) {
+    console.log(message);
+}
+log("Ashish");
+let user1 = {
+    uid: 1,
+    name: "asjddj"
+};
+const add = (x, y) => x + y;
+// classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        console.log(`${this.name} is registered`);
+    }
+}
+const obj1 = new Person(123, "ashish");
+console.log(obj1.register());
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,167 @@
-let id =5
-id="ashish"
+// Basic types
+
+let id: number = 5;
+let company: string = "PS";
+let isPublished: boolean = true;
+let X: any = "Hello";
+X = 123;
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, "Hello"];
+
+//tuple
+
+let person: [number, string, boolean] = [1, "brad", true];
+
+// tuple array
+
+let employee: [number, string][];
+
+employee = [
+  [1, "ash"],
+  [2, "pan"],
+  [3, "day"],
+];
+
+// union
+
+let id1: string | number = 22;
+
+// enum
+
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+console.log(Direction1.Left);
+
+
+// Objects
+
+type User = {
+    id: number,
+    name:string
+}
+
+const user :User= {
+
+    id:1,
+    name:"Ashish"
+
+}
+
+// Type Assertion 
+
+let cid: any =1
+
+// let customerId = <number>cid
+
+let customerId = cid as number
+
+
+// Functions
+
+function addNumber(x:number,y:number):number {
+
+    return x+y
+}
+
+
+console.log(addNumber(2,3))
+
+// void
+
+function log(message: string| number):void {
+
+    console.log(message)
+}
+
+log("Ashish")
+
+// Interfaces
+
+interface newUser{
+
+    readonly uid:number
+    name:string
+    age?:number
+
+}
+
+let user1:newUser={
+
+    uid:1,
+    name:"asjddj"
+}
+
+// interface vs type ????????
+
+// interface with functions
+
+
+interface MathFunc {
+
+    (x:number, y:number):number
+}
+
+const add:MathFunc = (x:number, y:number):number => x+y
+
+
+// classes
+
+interface personInterface{
+    id :number
+    name:string
+    register(): string
+}
+
+class Person implements personInterface {
+
+    id:number
+    name:string
+    constructor(id:number,name:string) {
+
+        this.id =id
+        this.name =name
+    }
+
+    register() {
+        return "Hello Hello"
+    }
+}
+
+const obj1 = new Person(123,"ashish")
+
+console.log(obj1.register())
+
+// Subclasses
+
+class Employee extends Person {
+
+    position:string
+
+    constructor(id:number, name: string, position:string) {
+
+        super(id,name)
+        this.position =position
+    }
+}
+
+
+const emp1 = new Employee(3,"Ashish","hdhdhd")
+
+
+// Generics 
+
+function getArray<T>(items:T[]):T[] {
+
+    return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1,2,3])
+let strArray = getArray<string>(["Asnsnsh","kasfhdk"])
+
+numArray.push(100)
